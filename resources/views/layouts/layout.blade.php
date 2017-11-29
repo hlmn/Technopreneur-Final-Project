8ba7144c905960e846a25d60cec0f3ee4bbc5651
Muhammad Hilman <hlmn.hg@gmail.com>
2017-11-29 22:32:11 +0700

    title

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Pendent Store a Ecommerce Online Shopping Category Flat Bootstarp Resposive Website Template | About :: w3layouts</title>
+<title>InvestNation</title>
 <!-- for-mobile-apps -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
